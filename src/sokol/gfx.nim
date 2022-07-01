9960816d9e3f08f838d1edc24e5a2d7cfa3ea074
Karl Fleischmann <fleischmann.karl@mailbox.org>
2022-07-01 14:02:43 +0200

    gfx.nim: Drop duplicate libraries for linking
    Firstly these are defined in src/sokol/app.nim and secondly these are
    not directly used in gfx.nim.

--- src/sokol/gfx.nim
+++ src/sokol/gfx.nim
@@ -1169,7 +1169,7 @@ elif defined macosx:
     {.passl:"-framework Metal -framework MetalKit".}
 elif defined linux:
   {.passc:"-DSOKOL_GLCORE33".}
-  {.passl:"-lX11 -lXi -lXcursor -lGL -lm -ldl -lpthread".}
+  {.passl:"-lGL".}
 else:
   error("unsupported platform")
 
