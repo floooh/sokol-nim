9414316fc1cfe27b5f1a9f3c48b6b77081188599
Andre Weissflog <floooh@gmail.com>
2022-07-02 14:18:00 +0200

    remove the D3D11_STANDARD_MULTISAMNPLE_PATTERN override in sokol_defines.h, doesn't seem to be needed

--- src/sokol/c/sokol_defines.h
+++ src/sokol/c/sokol_defines.h
@@ -2,15 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//------------------------------------------------------------------------------
-
-#if defined(_WIN32) && defined(__GNUC__)
-    // mingw64 gcc headers for D3D11 are missing this:
-    #define D3D11_STANDARD_MULTISAMPLE_PATTERN (0xffffffff)
-#endif
-
-//------------------------------------------------------------------------------
-
 #define SOKOL_ASSERT(x) ((x)?(1):(sokol_assert_failed(__FILE__,__LINE__,#x),0))
 
 static inline void
