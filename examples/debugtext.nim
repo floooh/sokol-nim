793486d544f4266b39c448be8bb56eb17d7423c9
leiserfg <leiserfg@gmail.com>
2026-08-09 23:12:36 +0200

    Fixes on debugtext

--- examples/debugtext.nim
+++ examples/debugtext.nim
@@ -8,11 +8,10 @@ import sokol/app as sapp
 import sokol/debugtext as sdtx
 import sokol/glue as sglue
 
-const passAction = PassAction(
-  colors: [
-    ColorAttachmentAction(loadAction: loadActionClear, clearValue: (0, 0.125, 0.25, 1))
-  ]
-)
+let passAction = block:
+  var p = PassAction()
+  p.colors[0]= ColorAttachmentAction(loadAction: loadActionClear, clearValue: (0, 0.125, 0.25, 1))
+  p
 
 proc init() {.cdecl.} =
   sg.setup(sg.Desc(
@@ -21,17 +20,18 @@ proc init() {.cdecl.} =
   ))
 
   # setup sokol/debugtext
-  sdtx.setup(sdtx.Desc(
-    fonts: [
-      sdtx.fontKc853(),
-      sdtx.fontKc854(),
-      sdtx.fontZ1013(),
-      sdtx.fontCpc(),
-      sdtx.fontC64(),
-      sdtx.fontOric()
-    ],
-    logger: sdtx.Logger(fn: slog.fn),
-  ))
+  sdtx.setup:
+    var desc = sdtx.Desc(
+      logger: sdtx.Logger(fn: slog.fn),
+    )
+    desc.fonts[0] = sdtx.fontKc853()
+    desc.fonts[1] = sdtx.fontKc854()
+    desc.fonts[2] = sdtx.fontZ1013()
+    desc.fonts[3] = sdtx.fontCpc()
+    desc.fonts[4] = sdtx.fontC64()
+    desc.fonts[5] = sdtx.fontOric()
+    desc
+
 
 proc printFont(fontIndex: int32, title: cstring, r: uint8, g: uint8, b: uint8) =
   sdtx.font(fontIndex)
@@ -49,12 +49,12 @@ proc frame() {.cdecl.} =
     sdtx.canvas(sapp.widthf()*0.5, sapp.heightf()*0.5)
     sdtx.origin(0, 2)
     sdtx.home()
-    print_font(0, "KC85/3:\n",      0xf4, 0x43, 0x36)
-    print_font(1, "KC85/4:\n",      0x21, 0x96, 0xf3)
-    print_font(2, "Z1013:\n",       0x4c, 0xaf, 0x50)
-    print_font(3, "Amstrad CPC:\n", 0xff, 0xeb, 0x3b)
-    print_font(4, "C64:\n",         0x79, 0x86, 0xcb)
-    print_font(5, "Oric Atmos:\n",  0xff, 0x98, 0x00)
+    printFont(0, "KC85/3:\n",      0xf4, 0x43, 0x36)
+    printFont(1, "KC85/4:\n",      0x21, 0x96, 0xf3)
+    printFont(2, "Z1013:\n",       0x4c, 0xaf, 0x50)
+    printFont(3, "Amstrad CPC:\n", 0xff, 0xeb, 0x3b)
+    printFont(4, "C64:\n",         0x79, 0x86, 0xcb)
+    printFont(5, "Oric Atmos:\n",  0xff, 0x98, 0x00)
 
     sg.beginPass(Pass(action: passAction, swapchain: sglue.swapchain()))
     sdtx.draw()
@@ -72,6 +72,6 @@ sapp.run(sapp.Desc(
   width: 1024,
   height: 600,
   windowTitle: "debugtext.nim",
-  icon: IconDesc(sokol_default: true),
+  icon: IconDesc(sokolDefault: true),
   logger: sapp.Logger(fn: slog.fn),
 ))
